8b2c9427e1c506ee94759e7262c04524eb26153e
Cameron-Boivin <cboivin@student.bridgew.edu>
2025-10-16 14:43:18 -0400

    Changed up the Restaurant.java

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,14 +1,30 @@
-public class Restaurant  extends Address{
-<<<<<<< HEAD
+import javax.swing.*;
+
+public class Restaurant {
+
     private String name;
     private String[] menuItems;
+    private Integer[] menuPrices;
 
-
-=======
-
-    private String name;
-    private String[] menu;
-    private int prices;
->>>>>>> 7409eeae53b701e3ce2ac48f3eb376a9c0a6b00f
+    public void setName(String n){
+        name = n;
+    }
+    public void assignItem(String mi, int mp){
+        String[] temp1 = menuItems;
+        Integer[] temp2 = menuPrices;
+        menuItems = new String[menuItems.length+1];
+        menuPrices = new Integer[menuPrices.length+1];
+        for (int i = 0; i != temp1.length; i++){
+            menuItems[i] = temp1[i];
+            menuPrices[i] = temp2[i];
+        }
+        menuItems[menuItems.length-1] = mi;
+        menuPrices[menuPrices.length-1] = mp;
+    }
+    public void itemList(){
+        for (int i = 0; i < menuItems.length; i++){
+            System.out.println("Item " + i + " is " + menuItems[i] + " which is priced at " + menuPrices[i] + ".");
+        }
+    }
 
 }
