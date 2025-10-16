8d2f66809002c7758abd87a8f8febc3f1a97ed61
j4arruda <157047109+j4arruda@users.noreply.github.com>
2025-10-16 14:56:30 -0400

    rest update

--- src/Cart.java
+++ src/Cart.java
@@ -1,5 +1,10 @@
+import java.util.HashMap;
+
 public class Cart extends Restaurant {
     private String add;
     private String remove;
 
+    public Cart(String name, String address, HashMap<String, Double> menu) {
+        super(name, address, menu);
+    }
 }
