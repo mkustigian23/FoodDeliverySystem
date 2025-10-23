89000f1b0c252828ab7e9f5f79d5c4ac24842de4
D1Fortune <dawensfort89@gmail.com>
2025-10-22 23:08:32 -0400

    Create Login.java

---
+++ src/Login.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Login {
+    private int phoneNum;
+    private String email;
+    private String username;
+    private String password;
+
+    public void loginInfo(){
+        System.out.println("Insert User Info");
+    }
+}
