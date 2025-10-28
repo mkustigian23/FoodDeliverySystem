da1fc39e7552fd8a7b149d795c7ff4872501a15c
Cameron-Boivin <cboivin@student.bridgew.edu>
2025-10-28 14:53:17 -0400

    Updated name and Login with getters and setters

--- src/Login.java
+++ src/Login.java
@@ -5,8 +5,40 @@ public class Login {
     private String email;
     private String username;
     private String password;
+    private String accType;
 
     public void loginInfo(){
         System.out.println("Insert User Info");
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setPhoneNum(int phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+    public int getPhoneNum() {
+        return phoneNum;
+    }
+    public void setAccType(String accType) {
+        this.accType = accType;
+    }
+    public String getAccType() {
+        return accType;
     }
 }
