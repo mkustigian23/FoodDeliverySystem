e3f72b0f44eaea55106ce736eeff494903fb3024
D1Fortune <dawensfort89@gmail.com>
2025-10-28 14:47:54 -0400

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -1,12 +1,37 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Label;
+
 
 public class Login {
-    private int phoneNum;
-    private String email;
-    private String username;
-    private String password;
+    JLabel usernameLabel = new JLabel("Username:");
+    JLabel passwordLabel = new JLabel("Password:");
+    JPasswordField passwordField = new JPasswordField();
+    JButton loginButton = new JButton("Login");
 
-    public void loginInfo(){
-        System.out.println("Insert User Info");
+    public void setLayout(new FlowLayout()){
+
+    }
+
+    public void add(usernameLabel) {
+        this.
+    }
+
+    add(usernameField);
+
+    public void add(passwordLabel) {
+
+    }
+
+    add(passwordField);
+
+    void add(loginButton);
+
+        loginButton.addActionListener(e ->)
+
+    {
+        Label usernameField = new Label();
+        String username = usernameField.getText();
+        String password = new String(passwordField.getPassword());
     }
 }
